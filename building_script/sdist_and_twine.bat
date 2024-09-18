9b0358490f1bb46acdb7f971a7075fa65a8fe005
Andrew McSparron <Amcsparron@albanyny.gov>
2024-09-18 10:39:32 -0400

    Update sdist_and_twine.bat to use python -m build
    Modified the script to run `python -m build` for all Python versions, simplifying the build process. Also, added missing dependencies to `requirements.txt` to ensure all necessary packages are installed.

--- building_script/sdist_and_twine.bat
+++ building_script/sdist_and_twine.bat
@@ -11,13 +11,13 @@ echo pwd changed to %cd%
 
 REM need LICENSE.txt README.md setup.cfg setup.py - see https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
 REM DONT FORGET TO UPDATE setup.py, push commit to remote, and create a new release!! THEN run this!!!
-echo running sdist setup
+echo running dist setup
 
 REM THIS IS THE COMMAND FOR PYTHON 3.10? and before.
 REM python setup.py sdist
 
 REM THIS IS THE COMMAND FOR PYTHON 3.12 and AFTER
-python -m build sdist
+python -m build
 
 REM You can upload your distributions to TestPyPI using twine by specifying the --repository flag:
 REM twine upload --repository testpypi dist/*
