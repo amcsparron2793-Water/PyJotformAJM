4e50df9c78327df5b2b1a9ec033d7b43e82dd6c2
Andrew McSparron <Amcsparron@albanyny.gov>
2024-09-18 10:58:56 -0400

    Add main module import and update dependencies.
    This commit includes importing the main module in `__init__.py` to facilitate easier access and usage of its functionalities. Additionally, it updates `setup.py` to include the `ApiKeyAJM` dependency necessary for the implementation.

--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ def get_property(prop, project):
     result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open(project + '/_version.py').read())
     return result.group(1)
 
+
 setup(
     name=project_name,
     version=get_property('__version__', project_name),
     packages=['PyJotformAJM'],
+    install_requires=['jotform', 'ApiKeyAJM'],
     url='https://github.com/amcsparron2793-Water/PyJotformAJM',
     download_url=f'https://github.com/amcsparron2793-Water/PyJotformAJM/archive/refs/tags/{get_property("__version__", project_name)}.tar.gz',
     keywords=[],
